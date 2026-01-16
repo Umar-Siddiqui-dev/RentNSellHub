59215655d592181617d33528fe8a594c44be40ce
Umar Siddiqui <85808160+SidHub99@users.noreply.github.com>
2026-01-16 17:02:13 +0500

    added DBurl inside schema

--- backend/lib/prisma.js
+++ backend/lib/prisma.js
@@ -1,13 +1,14 @@
 import { PrismaClient } from "@prisma/client";
 
 // Pass your DATABASE_URL here
-const prisma = new PrismaClient({
-  // datasources: {
-  //   db: {
-  //     url: process.env.DATABASE_URL, // required in Prisma 7
-  //   },
-  // },
-  datasourceUrl: process.env.DATABASE_URL,
-});
+// const prisma = new PrismaClient({
+//   // datasources: {
+//   //   db: {
+//   //     url: process.env.DATABASE_URL, // required in Prisma 7
+//   //   },
+//   // },
+//   datasourceUrl: process.env.DATABASE_URL,
+// });
 
+const prisma = new PrismaClient();
 export default prisma;
